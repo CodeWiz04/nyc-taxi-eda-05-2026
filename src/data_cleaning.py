2eed308354e755a7a411f1439e103a98ba843275
Shafan Ali <muhammadshafan2004@gmail.com>
2026-07-24 11:09:27 +0500

    Add duplicate removal logic to data cleaning

---
+++ src/data_cleaning.py
@@ -0,0 +1,9 @@
+import pandas as pd
+#  Load Data
+df=pd.read_parquet("data/raw/yellow_tripdata_2026-05.parquet")
+
+#  Duplicates Removal
+duplicate_count=df.duplicated().sum()
+print(f"Duplicate rows:{duplicate_count}")
+
+df=df.drop_duplicates()
